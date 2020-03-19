b6b69c346840f16d67d5a82a540a464300e08b64
AssociationSirius <olivier@asso-sirius.org>
2020-03-19 04:29:05 +0100

    Création YUV_WRAPPER

---
+++ src/mediaModule/yuv_wrapper.hpp
@@ -0,0 +1,28 @@
+/*
+* This source is the property of Immersive Adventure
+* http://immersiveadventure.net/
+*
+* It has been developped by part of the LSS Team.
+* For further informations, contact:
+*
+* albertpla@immersiveadventure.net
+*
+* This source code mustn't be copied or redistributed
+* without the authorization of Immersive Adventure
+* (c) 2017 - all rights reserved
+*
+*/
+
+
+#ifndef __YUV_WRAPPER_HPP__
+#define __YUV_WRAPPER_HPP__
+
+#include <GL/glew.h>
+
+struct YUV_WRAPPER {
+    GLint TexY;
+    GLint TexU;
+    GLint TexV;
+};
+
+#endif  // __YUV_WRAPPER_HPP__
