81a947f25903b486d106aa335df16de384b08098
AssociationSirius <olivier@asso-sirius.org>
2020-04-09 07:57:29 +0200

    Implémentation du manager de SkyDisplay

---
+++ src/coreModule/skydisplay_mgr.cpp
@@ -0,0 +1,221 @@
+/*
+ * Spacecrafter astronomy simulation and visualization
+ *
+ * Copyright (C) 2016 of the LSS Team & Association Sirius
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Spacecrafter is a free open project of the LSS team
+ * See the TRADEMARKS file for free open project usage requirements.
+ *
+ */
+
+#include <iostream>
+#include <iterator>
+
+#include "coreModule/skydisplay_mgr.hpp"
+#include "tools/log.hpp"
+
+SkyDisplayMgr::SkyDisplayMgr()
+{
+	//baseColor=Vec3f(0.f, 0.f, 0.f);
+}
+
+void SkyDisplayMgr::draw(const Projector *prj,const Navigator *nav, Vec3d equPos, Vec3d oldEquPos)
+{
+	for (auto it=m_map.begin(); it!=m_map.end(); ++it) {
+		it->second->draw(prj, nav, equPos, oldEquPos);
+	}
+}
+
+void SkyDisplayMgr::drawPerson(const Projector *prj,const Navigator *nav)
+{
+	if ((personAL!=nullptr) && (personEQ!=nullptr)) {
+		personAL->draw(prj,nav);
+		personEQ->draw(prj,nav);
+	}
+}
+
+
+SkyDisplayMgr::~SkyDisplayMgr()
+{
+	for (auto it=m_map.begin(); it!=m_map.end(); ++it) {
+		cLog::get()->write("SkyDisplayMgr : delete " + getSkyName(it->first) , LOG_TYPE::L_INFO);
+		delete it->second;
+	}
+}
+
+void SkyDisplayMgr::update(int delta_time)
+{
+	for (auto it=m_map.begin(); it!=m_map.end(); ++it) {
+		it->second->update(delta_time);
+	}
+}
+
+void SkyDisplayMgr::setFont(float font_size, const std::string& font_name)
+{
+	// for (auto it=m_map.begin(); it!=m_map.end(); ++it) {
+	// 	it->second->setFont(font_size, font_name);
+	// }
+}
+
+
+void SkyDisplayMgr::flipFlagShow(SKYDISPLAY_NAME nameObj)
+{
+	for (auto it=m_map.begin(); it!=m_map.end(); ++it) {
+		if (it->first==nameObj) {
+			it->second->flipFlagShow();
+			return;
+		}
+	}
+	cLog::get()->write("SkyDisplayMgr error : flipFlagShow not found " + getSkyName(nameObj) , LOG_TYPE::L_WARNING);
+}
+
+
+void SkyDisplayMgr::setFlagShow(SKYDISPLAY_NAME nameObj, bool a)
+{
+	for (auto it=m_map.begin(); it!=m_map.end(); ++it) {
+		if (it->first==nameObj) {
+			it->second->setFlagShow(a);
+			return;
+		}
+	}
+	cLog::get()->write("SkyDisplayMgr error : setFlagShow not found " + getSkyName(nameObj) , LOG_TYPE::L_WARNING);
+}
+
+
+bool SkyDisplayMgr::getFlagShow(SKYDISPLAY_NAME nameObj)
+{
+	for (auto it=m_map.begin(); it!=m_map.end(); ++it) {
+		if (it->first==nameObj) {
+			return it->second->getFlagShow();
+		}
+	}
+	cLog::get()->write("SkyDisplayMgr error : getFlagShow not found " + getSkyName(nameObj) , LOG_TYPE::L_WARNING);
+	return false;
+}
+
+
+void SkyDisplayMgr::setColor(SKYDISPLAY_NAME nameObj, const Vec3f& c)
+{
+	for (auto it=m_map.begin(); it!=m_map.end(); ++it) {
+		if (it->first==nameObj) {
+			it->second->setColor(c);
+        break;
+        }
+	}
+}
+
+const Vec3f& SkyDisplayMgr::getColor(SKYDISPLAY_NAME nameObj)
+{
+	for (auto it=m_map.begin(); it!=m_map.end(); ++it) {
+		if (it->first==nameObj) {
+			return it->second->getColor();
+		}
+	}
+	cLog::get()->write("SkyDisplayMgr error : getColor not found " + getSkyName(nameObj) , LOG_TYPE::L_WARNING);
+	return baseColor;
+}
+
+void SkyDisplayMgr::Create(SKYDISPLAY_NAME nameObj)
+{
+	SkyDisplay* tmp=nullptr;
+	auto it=m_map.find(nameObj);
+
+	//si l'itérateur ne vaut pas map.end(), cela signifie que que la clé à été trouvée
+	if(it!=m_map.end()) {
+		cLog::get()->write("SkyDisplayMgr SkyGrid already create " + getSkyName(nameObj) , LOG_TYPE::L_ERROR);
+		return;
+	}
+	cLog::get()->write("SkyDisplayMgr creating "+ getSkyName(nameObj) , LOG_TYPE::L_INFO);
+
+	//switch (nameObj) {
+	switch (nameObj) {
+		case SKYDISPLAY_NAME::SKY_PERSONAL :
+			tmp= new SkyPerson(SkyDisplay::AL);
+			m_map[nameObj]= tmp;
+			personAL = tmp;
+			return;
+			break;
+
+		case SKYDISPLAY_NAME::SKY_PERSONEQ :
+			tmp= new SkyPerson(SkyDisplay::EQ);
+			m_map[nameObj]= tmp;
+			personEQ = tmp;
+			return;
+			break;
+
+		case SKYDISPLAY_NAME::SKY_NAUTICAL :
+			tmp= new SkyNautic(SkyDisplay::AL);
+			m_map[nameObj]= tmp;
+			return;
+			break;
+
+		case SKYDISPLAY_NAME::SKY_NAUTICEQ :
+			tmp= new SkyNautic(SkyDisplay::EQ);
+			m_map[nameObj]= tmp;
+			return;
+			break;
+
+		case SKYDISPLAY_NAME::SKY_OBJCOORDS :
+			tmp= new SkyCoords();
+			m_map[nameObj]= tmp;
+			return;
+			break;
+
+		case SKYDISPLAY_NAME::SKY_MOUSECOORDS :
+			tmp= new SkyCoords();
+			m_map[nameObj]= tmp;
+			return;
+			break;
+
+		case SKYDISPLAY_NAME::SKY_ANGDIST :
+			tmp= new SkyAngDist();
+			m_map[nameObj]= tmp;
+			return;
+			break;
+
+		case SKYDISPLAY_NAME::SKY_LOXODROMY :
+			tmp= new SkyLoxodromy();
+			m_map[nameObj]= tmp;
+			return;
+			break;
+
+		case SKYDISPLAY_NAME::SKY_ORTHODROMY :
+			tmp= new SkyOrthodromy();
+			m_map[nameObj]= tmp;
+			return;
+			break;
+
+		default: // inconnue
+			break;
+	}
+}
+
+std::string SkyDisplayMgr::getSkyName(SKYDISPLAY_NAME nameObj)
+{
+    switch (nameObj) {
+        case SKYDISPLAY_NAME::SKY_PERSONAL : return "SkyPersonAL"; break;
+	    case SKYDISPLAY_NAME::SKY_PERSONEQ : return "SkyPersonEQ"; break;
+	    case SKYDISPLAY_NAME::SKY_NAUTICAL : return "SkyNauticAL"; break;
+	    case SKYDISPLAY_NAME::SKY_NAUTICEQ : return "SkyNauticEQ"; break;
+	    case SKYDISPLAY_NAME::SKY_ORTHODROMY: return "SkyOrthodromy"; break;
+	    case SKYDISPLAY_NAME::SKY_LOXODROMY : return "SkyLoxodromy"; break;
+	    case SKYDISPLAY_NAME::SKY_OBJCOORDS: return "SkyObjCoords"; break;
+	    case SKYDISPLAY_NAME::SKY_MOUSECOORDS: return "SkymouseCoords"; break;
+	    case SKYDISPLAY_NAME::SKY_ANGDIST: return "SkyAngDist"; break;
+        default: return "None"; break;
+    }
+}
---
+++ src/coreModule/skydisplay_mgr.hpp
@@ -0,0 +1,81 @@
+/*
+ * Spacecrafter astronomy simulation and visualization
+ *
+ * Copyright (C) 2016 of the LSS Team & Association Sirius
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Spacecrafter is a free open project of the LSS team
+ * See the TRADEMARKS file for free open project usage requirements.
+ *
+ */
+
+
+#ifndef SKYDISPLAY_MGR_HPP
+#define SKYDISPLAY_MGR_HPP
+
+#include <map>
+#include <string>
+
+#include "skyDisplay.hpp"
+#include "tools/s_font.hpp"
+#include "coreModule/projector.hpp"
+#include "navModule/navigator.hpp"
+#include "tools/fader.hpp"
+#include "coreModule/core_common.hpp"
+
+// enum class GRID_TYPE : char {
+// 	GRID_EQUATORIAL,
+// 	GRID_ECLIPTIC,
+// 	GRID_GALACTIC,
+// 	GRID_ALTAZIMUTAL,
+// 	GRID_UNKNOWN
+// };
+
+
+class SkyDisplayMgr {
+public:
+	SkyDisplayMgr();
+	~SkyDisplayMgr();
+	SkyDisplayMgr(SkyDisplayMgr const &) = delete;
+	SkyDisplayMgr& operator = (SkyDisplayMgr const &) = delete;
+
+	int size() {
+		return m_map.size();
+	};
+	//Celle qui va créer les objets
+	void Create(SKYDISPLAY_NAME nameObj);
+	void draw(const Projector *prj,const Navigator *nav, Vec3d equPos, Vec3d oldEquPos);
+	void drawPerson(const Projector *prj,const Navigator *nav);
+	void update(int delta_time);
+
+	void setFont(float font_size, const std::string& font_name);
+
+	void setColor(SKYDISPLAY_NAME nameObj, const Vec3f& c);
+	const Vec3f& getColor(SKYDISPLAY_NAME nameObj);
+
+	//! change FlagShow: inverse la valeur du flag
+	void setFlagShow(SKYDISPLAY_NAME nameObj, bool b);
+	bool getFlagShow(SKYDISPLAY_NAME nameObj);
+	void flipFlagShow(SKYDISPLAY_NAME nameObj);
+
+private:
+    std::string getSkyName(SKYDISPLAY_NAME nameObj);
+	std::map<SKYDISPLAY_NAME ,SkyDisplay*> m_map;
+	SkyDisplay* personAL = nullptr;
+	SkyDisplay* personEQ = nullptr;
+	Vec3f baseColor=Vec3f(0.f, 0.f, 0.f);
+};
+#endif //SKYDISPLAY_MGR_HPP
