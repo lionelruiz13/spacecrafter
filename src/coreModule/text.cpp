a05803e66fb02cf4fedc04059998b398a68c8474
AssociationSirius <olivier@asso-sirius.org>
2020-04-11 17:57:25 +0200

    s_font::printHorizontal n'utilise plus cache et outline

--- src/coreModule/text.cpp
+++ src/coreModule/text.cpp
@@ -99,5 +99,5 @@ void Text::draw(const Projector* prj, s_font *textFont[])
 		case FontSize::T_XX_LARGE: tmp = textFont[6]; break;
 		default: tmp = textFont[3]; break; // cas medium
 	}
-	tmp->printHorizontal(prj, altitude, azimuth, text,textColor, 1, true/*, true*/);
+	tmp->printHorizontal(prj, altitude, azimuth, text,textColor); //, 1, true/*, true*/);
 }
