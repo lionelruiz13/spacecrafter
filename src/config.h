761e4dc25089d3002fd81b1abe4e6a9ec89d813a
AssociationSirius <olivier@asso-sirius.org>
2020-04-13 05:41:01 +0200

    VERSION 20.4.12

--- src/config.h
+++ src/config.h
@@ -6,8 +6,8 @@
 #define _CONFIG_H_
 
 #define PACKAGE  "spacecrafter"
-#define VERSION "20.4.1"
-#define APP_NAME "spacecrafter 20.4.1"
+#define VERSION "20.4.12"
+#define APP_NAME "spacecrafter 20.4.12"
 #define APP_LOWER_NAME "spacecrafter"
 #define USER_NAME "LSS Open Project"
 #define USER_EDITION "Planetarium Edition"
