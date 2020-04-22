55c77fd9178a425381e0b8b2d1419ce9a48970c6
N.Barile <n.barile.57@gmail.com>
2020-04-22 11:39:32 +0200

    Ajout de Objet dans core

--- util/CorePrototype/core.cpp
+++ util/CorePrototype/core.cpp
@@ -1,7 +1,19 @@
 #include"core.hpp"
 
-Core::Core() {}
-Core::~Core() {}
+Core::Core() {
+    obj1 = new Objet("obj1");
+    obj2 = new Objet("obj2");
+    obj3_universe = new Objet("obj3_universe");
+    obj4_galaxy = new Objet("obj4_galaxy");
+    camera = new Objet("camera");
+}
+Core::~Core() {
+    delete obj1;
+    delete obj2;
+    delete obj3_universe;
+    delete obj4_galaxy;
+    delete camera;
+}
 
 void Core::init() {
     std::cout << "initialisation core" << std::endl;
@@ -9,10 +21,20 @@ void Core::init() {
 
 void Core::update() {
     std::cout << "update core" << std::endl;
+    obj1->update();
+    obj2->update();
+    obj3_universe->update();
+    obj4_galaxy->update();
+    camera->update();
 }
 
 void Core::draw() {
     std::cout << "draw core" << std::endl;
+    obj1->draw();
+    obj2->draw();
+    obj3_universe->draw();
+    obj4_galaxy->draw();
+    camera->draw();
 }
 
 void Core::setFlagSelectedObjectPointer() {
