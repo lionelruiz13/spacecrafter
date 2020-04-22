1a3e4d17ccbb2c7719ac4787e9ac7348f1b064af
N.Barile <n.barile.57@gmail.com>
2020-04-22 11:39:38 +0200

    Tests objet

--- util/CorePrototype/main.cpp
+++ util/CorePrototype/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include"app.hpp"
 
+#include<iostream>
+
 int main() {
     App* app = new App();
 
-    app->start_main_loop();
+    std::cout << " --- DRAW --- " << std::endl;
+    app->draw();
+    std::cout << std::endl << " --- UPDATE --- " << std::endl;
+    app->update();
 
     return 0;
 }
