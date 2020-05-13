c062f8f998138d50c604c3dbe5f4e12552b0747f
AssociationSirius <olivier@asso-sirius.org>
2020-05-13 07:06:59 +0200

    Commentaires Doxygen

--- src/coreModule/illuminate_mgr.hpp
+++ src/coreModule/illuminate_mgr.hpp
@@ -2,6 +2,7 @@
  * Spacecrafter astronomy simulation and visualization
  *
  * Copyright (C) 2015 of the LSS Team & Association Sirius
+ * Copyright (C) 2020 of the LSS Team & Association Sirius
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -36,7 +37,16 @@ class Navigator;
 class ConstellationMgr;
 
 /*! \class IlluminateMgr
-  * \brief IlluminateMgr handles all illumiante stars for better stars visualisation.
+  * \brief handles all illuminate stars from Hipparcos catalog for better stars visualisation.
+  * \author Olivier NIVOIX
+  * \date 13 mai 2020
+  * 
+  * @section DESCRIPTION
+  * Cette classe stocke dans la structure illumianteGrid un illuminate, objet visant à recouvrir une étoile d'une texture 
+  * afin de la rendre plus visible sous le dôme.
+  * 
+  * Les étoiles sont issues du catalogue Hipparcos.
+  * 
   */
 class IlluminateMgr {
 public:
@@ -45,17 +55,17 @@ public:
 	IlluminateMgr(IlluminateMgr const &) = delete;
 	IlluminateMgr& operator = (IlluminateMgr const &) = delete;
 
-	//! search by name and return an Illuminate object
-	Illuminate *search(unsigned int name);
-
+	// indique la taille d'affichage des illuminates par défaut 
 	void setDefaultSize(double v) {
 		defaultSize =v;
 	}
 
+	// construit l'illuminate d'une étoile 
 	void load(int num, double size, double rotation);
+	// construit l'illuminate d'une étoile en indiquant la couleur utilisée
 	void load(int num, const Vec3f& _color, double _size, double rotation);
 
-	//! remove user added Illuminate and optionally unhide the original of the same name
+	//! remove user added Illuminate
 	void remove(unsigned int name);
 
 	//! remove all user added Illuminate
@@ -64,33 +74,39 @@ public:
 	//! Draw all the Illuminate
 	void draw(Projector *prj, const Navigator *nav);
 
-	//! change la texture des illuminate par le fichier proposé en paramètre
+	//! change la texture par defaut des illuminates par le fichier proposé en paramètre
 	void changeTex(const std::string& fileName);
 	//!	supprime la texture définie par l'utilisateur
 	void removeTex();
 
 	//! charge toutes les étoiles de l'asterism d'une constellation
 	void loadConstellation(const std::string& abbreviation, double size, double rotation);
+	//! charge toutes les étoiles de l'asterism d'une constellation en imposant la couleur du groupement
 	void loadConstellation(const std::string& abbreviation, const Vec3f& color, double size, double rotation);
+	//! charge toutes les étoiles de tous les asterismes d'une sky_culture
 	void loadAllConstellation(double size, double rotation);
 
 	//! supprime toutes les étoiles de l'asterism d'une constellation
 	void removeConstellation(const std::string& abbreviation);
+	//! supprime toutes les étoiles de tous les asterismes d'une sky_culture
 	void removeAllConstellation();
 
 private:
+	//! search by name and return an Illuminate object
+	Illuminate *search(unsigned int name);
+
 	//! Load an individual Illuminate with all data
 	void loadIlluminate(unsigned int name, double ra, double de, double angular_size, double r, double g, double b, double tex_rotation);
 
-	std::vector<Illuminate*> illuminateArray; 		//!< The Illuminate list
+	std::vector<Illuminate*> illuminateArray; 		//!< The Illuminate vector
 	std::vector<Illuminate*>* illuminateZones;		//!< array of Illuminate vector with the grid id as array rank
-	LittleGrid illuminateGrid;					//!< Grid for opimisation
-	double defaultSize;
+	LittleGrid illuminateGrid;					//!< Grid for display opimisation
+	double defaultSize;							//!< defautl Size from illuninate if not precised
 
-	shaderProgram* shaderIllum;
-	HipStarMgr* hip_stars = nullptr;
-	Navigator* navigator = nullptr;
-	ConstellationMgr* asterism= nullptr;
+	shaderProgram* shaderIllum;					//!< shader how draw all illuminate
+	HipStarMgr* hip_stars = nullptr;			//!< provide acces point to HipStarMgr
+	Navigator* navigator = nullptr;				//!< provide acces point to Navigator
+	ConstellationMgr* asterism= nullptr;		//!< provide acces point to ConstellationMgr
 
 	DataGL Illum;
 	std::vector<float> illumPos;
