2c7cad5e87b3a8b75a71263830873b196b766a28
N.Barile <n.barile.57@gmail.com>
2020-05-27 18:43:56 +0200

    Ecriture Class FileReader

--- util/new_parser_scripts/FileReader.cpp
+++ util/new_parser_scripts/FileReader.cpp
@@ -1,9 +1,62 @@
 #include "FileReader.hpp"
+#include "DefineFile.hpp"
 
-FileReader::FileReader() {
+FileReader::FileReader() {}
+FileReader::~FileReader() {}
 
+char* FileReader::init(char* source, char* destination) {
+	//Fichier source
+	rstream = fopen(source, "r");
+	if(rstream == NULL) {
+		perror("Erreur d'ouverture du fichier source en lecture ");
+		exit(FOPEN_SOURCE_ERROR);	
+	}
+	
+	//Fichier html de destination
+	wstream = fopen(destination, "w");
+	if(rstream == NULL) {
+		fclose(rstream);
+		perror("Erreur d'ouverture du fichier html de destination en écriture ");
+		exit(FOPEN_DESTINATION_ERROR);
+	}
+	
+	//Buffer contenant la ligne courante
+	char* line;
+	//line = malloc(sizeof(*line) * BUFFER_SIZE);
+	if(line == NULL) {
+		perror("Erreur d'allocation du buffer ");
+		end(MALLOC_ERROR);
+	 }
+	realline = line;
+	return line;
 }
 
-FileReader::~FileReader() {
+void FileReader::end(int code) {
+	fclose(rstream);
+	fclose(wstream);
+	exit(code);
+}
 
+char* FileReader::nextline() {
+	char* line = realline;
+	if(fgets(line, BUFFER_SIZE, rstream) == NULL) {
+		perror("Erreur ou fin du fichier ");
+		end(FGETS_ERROR);
+	}
+	line = noblank(line);
+	return line;
+}
+
+char* FileReader::noblank(char* line) {
+	while(line[0] == SPACE || line[0] == TAB) line++;
+	return line;
+}
+
+char* FileReader::noblanknorline(char* line) {
+	line = noblank(line);
+	while (line[0] == NEWLINE) {
+		line = nextline();
+		line = noblank(line);
+	}
+	return line;
 }
--- util/new_parser_scripts/FileReader.hpp
+++ util/new_parser_scripts/FileReader.hpp
@@ -1,13 +1,25 @@
 #ifndef FILE_READER_HPP
 #define FILE_READER_HPP
 
+#include <stdio.h>
+#include <stdlib.h>
+
 class FileReader {
 public:
     FileReader();
 	~FileReader();
     
-private:
+    char* init(char* source, char* destination);    //Ouvre les fichiers et alloue le buffer de la ligne courante
+    void end(int code);                             //Termine le programme en fermant les fichiers
 
+    char* nextline();                               //Place le poineur à la ligne suivante du fichier
+    char* noblank(char* line);                      //Ignore tous les espaces et tabulations
+    char* noblanknorline(char* line);               //Ignore tous les espaces et les tabulations ainsi que les lignes qui ne contiennent que ça
+
+private:
+    FILE* rstream;      //Flux de lecture
+    FILE* wstream;      //Flux d'écriture
+    char* realline;     //Constante contenant la position initiale du pointeur du buffer
 };
 
 #endif
