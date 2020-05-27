24470f4f82bd8134a123f99572156a977d28e7e3
N.Barile <n.barile.57@gmail.com>
2020-05-27 18:44:07 +0200

    Ecriture Class FileWriter

--- util/new_parser_scripts/FileWriter.cpp
+++ util/new_parser_scripts/FileWriter.cpp
@@ -1,9 +1,75 @@
 #include "FileWriter.hpp"
+#include "FileReader.hpp"
+#include "TextToHtml.hpp"
+#include "DefineFile.hpp"
 
 FileWriter::FileWriter() {
-
+    Reader = new FileReader();
+    text = new TextToHtml();
 }
 
-FileWriter::~FileWriter() {
+FileWriter::~FileWriter() {}
 
+void FileWriter::writetext(char* text) {
+	if(fputs(text, wstream) == EOF) {
+		perror("Erreur d'écriture ");
+		Reader->end(FPUTS_ERROR);
+	}
+}
+
+void FileWriter::writenchar(char* line, int  n) {
+	if(!fwrite(line, n, 1, wstream)) {
+		perror("Erreur d'écriture ");
+		Reader->end(FWRITE_ERROR);
+	}
+}
+
+void FileWriter::writenewline() {
+	writetext(HTML_NEWLINE);
+}
+
+void FileWriter::writetextnchar(char* begin, char* line, int  n, char* end) {
+	writetext(begin);
+	writenchar(line, n);
+	writetext(end);
+}
+
+char* FileWriter::writehtml(char* line) {
+	line = Reader->noblanknorline(line);
+	int  i=0;
+	int  lastchar = 0;
+	while(line[i] != SEPARATOR_1 && line[i] != SEPARATOR_2) {
+		if(line[i] == NEWLINE) {
+			if (lastchar) writenchar(line, lastchar+1);
+			line = Reader->nextline();
+			if(line[0] != SEPARATOR_1 && line[0] != SEPARATOR_2 && !(text->checktag(line))) writenewline();
+			else return line;
+			i=0;//Réinitialise l'itérateur comme c'est une nouvelle ligne
+			lastchar=0;//Idem pour le dernier caractère utile
+			
+		}
+		if(line[i] != SPACE && line[i] != TAB) lastchar = i;
+		i++;
+	}
+	if (lastchar) writenchar(line, lastchar+1);
+	return line+i;
+}
+
+char* FileWriter::writetexthtml(char* begin, char* line, char* end) {
+	writetext(begin);
+	line = writehtml(line);
+	writetext(end);
+	return line;
+}
+
+char* FileWriter::writetextdescpart(char* begin, char* line, char* end) {
+	writetext(begin);
+	line = text->descpart(line);
+	writetext(end);
+	return line;
+}
+
+int FileWriter::argclose(int argopen) {
+	if(argopen) writetext(AFTER_ARGUMENT_LIST);
+	return 0;
 }
--- util/new_parser_scripts/FileWriter.hpp
+++ util/new_parser_scripts/FileWriter.hpp
@@ -1,13 +1,33 @@
 #ifndef FILE_WRITER_HPP
 #define FILE_WRITER_HPP
 
+#include <stdio.h>
+#include <stdlib.h>
+// #include "FileReader.hpp"
+// #include "TextToHtml.hpp"
+
+class FileReader;
+class TextToHtml;
+
 class FileWriter {
 public:
     FileWriter();
 	~FileWriter();
     
-private:
+    void writetext(char* text);                                         //Ecrit du texte dans le fichier
+    void writenchar(char* line, int  n);                                //Ecrit les n premier caractères
+    void writenewline();                                                //Saute une ligne en html
+    void writetextnchar(char* begin, char* line, int  n, char* end);    //Permet d'écrire autour de writenchar(line, n)
+    char* writehtml(char* line);                                        //Ecrit dans le html et s'arrête à un des deux séparateur ou une balise après un saut de ligne (n'écrit pas les espaces après la balise et avant la fin)
+    char* writetexthtml(char* begin, char* line, char* end);            //Permet d'écrire autour de writehtml(line)
+    char* writetextdescpart(char* begin, char* line, char* end);        //Permet d'écrire autour de writetextdescpart(line)
+    int argclose(int argopen);                                          //Ferme une liste d'arguments ouverte
 
+private:
+    FILE* rstream;      //Flux de lecture
+    FILE* wstream;      //Flux d'écriture
+    FileReader* Reader;
+    TextToHtml* text;
 };
 
 #endif
