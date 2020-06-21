f14ebbb7d2c2fab768b2a90d53dc0b340761e715
AssociationSirius <olivier@asso-sirius.org>
2020-06-21 18:34:58 +0200

    Fix copyright

--- src/tools/translator.cpp
+++ src/tools/translator.cpp
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 2005 Fabien Chereau
  * Copyright (C) 2009 Digitalis Education Solutions, Inc.
+ * Copyright (C) 2020 Association Sirius
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
