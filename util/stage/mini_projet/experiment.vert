02faec29322114412acaa5b0de15b6eaebdd0781
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2020-07-17 14:53:24 +0200

    mini_projet : cube rotate

--- util/stage/mini_projet/experiment.vert
+++ util/stage/mini_projet/experiment.vert
@@ -8,5 +8,6 @@ layout(location = 0) in vec3 vertexPos;
 uniform mat4 Mat;
 
 void main() {
+    mat4 test = mat4(0.5,0,0,0, 0,0.5,0,0, 0,0,0.5,0, 0,0,0,1);
     gl_Position = Mat * vec4(vertexPos, 1);
 }
--- util/stage/mini_projet/main.cpp
+++ util/stage/mini_projet/main.cpp
@@ -25,7 +25,7 @@ int main()
     signal(SIGTERM, sigTerm);
     Log->openLog(LOG_FILE::INTERNAL, "spacecrafter");
     window.initSDL();
-    window.createWindow("Experiment", 800, 600, 0, 3, false, "~/.spacecrafter/data/icon.bpm");
+    window.createWindow("Experiment", 600, 600, 0, 3, false, "~/.spacecrafter/data/icon.bpm");
 
     // varray->registerVertexBuffer(BufferType::SHAPE, BufferAccess::STATIC);
 
@@ -54,9 +54,8 @@ int main()
     shader->init("experiment.vert", "experiment.frag");
 
     std::cout << "2\n";
-    auto mvp = Mat4f::scaling(0.7);
-    mvp.print();
-    const auto move = Mat4f::xrotation(0.05) * Mat4f::yrotation(0.03) * Mat4f::zrotation(0.04);
+    auto mvp = Mat4f::scaling(0.7).translation(Vec3f(-0.5, -0.5, -0.5));
+    const auto rotate = Mat4f::yawPitchRoll(0.05, 0.03, 0.04);
 
     std::cout << "3\n";
     auto varray = std::make_unique<VertexArray>();
@@ -72,10 +71,11 @@ int main()
 
     std::cout << "start\n";
     while (opened) {
-        mvp = move * mvp;
+        mvp = rotate * mvp;
         Renderer::clearColor();
         std::cout << "\ec";
         mvp.print();
+        shader->use();
         shader->setUniform("Mat", mvp);
         Renderer::drawArrays(shader.get(), varray.get(), GL_QUADS, 0, 24);
         window.glSwapWindow();
