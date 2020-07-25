9e45f31af016c26aab8ff8b461c77c2f807de004
AssociationSirius <olivier.nivoix@gmail.com>
2020-07-25 10:54:38 +0200

    Fix win.z = 0

--- shaders/fisheye.glsl
+++ shaders/fisheye.glsl
@@ -44,6 +44,8 @@ vec4 fisheyeProject(vec3 invec, vec3 clipping_fov)
 	win.z = 2. * (abs(depth) - zNear) / (zFar-zNear) - 1.; // on pourrait calculer globalement 2/(zFar-zNear)
 
 	win.w = (a<0.9*M_PI) && (win.z != 0.0) ? 1. : 0.;
+	if (win.z == 0.0)
+		win.z = -1e30;
 
     return win;
 }
