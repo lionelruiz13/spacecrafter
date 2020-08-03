2eeb95b2d2157ad286b937b06ab54a90c722d4ef
AssociationSirius <olivier@asso-sirius.org>
2020-08-03 16:52:58 +0200

    Doc

--- src/tools/s_font.hpp
+++ src/tools/s_font.hpp
@@ -55,22 +55,40 @@ typedef struct {
 typedef std::map< std::string, renderedString_struct > renderedStringHash_t;
 typedef renderedStringHash_t::const_iterator renderedStringHashIter_t;
 
+/**
+ * \file s_font.hpp
+ * \brief transforms strings into texture
+ * \author Olivier NIVOIX
+ * \version 2
+ *
+ * \class s_font
+ * 
+ * \brief this class, based on the SDL2_ttf library, transforms a text character string into an available texture
+ * for OpenGL to display it in two ways:
+ * - display parallel to the horizon
+ * - right display, in the sky.
+ */
 class s_font {
 
 public:
+	//! crée une fonte de taille size_i utilisant le fichier fonte nommé ttfFileName
 	s_font(float size_i, const std::string& ttfFileName);
 	virtual ~s_font();
 
+	//! affiche un texte s droit au point M(x,y) de couleur Color à la position MVP avec upsidedown indiquant s'il est à l'endroit ou à l'envers
 	void print(float x, float y, const std::string& s, Vec4f Color, Mat4f MVP ,int upsidedown);
+	//! afficher un texte parallège à l'horizon en altitude azimuth
+	//! cache indique si l'on doit garder le texte en mémoire  
 	void printHorizontal(const Projector * prj, float altitude, float azimuth, const std::string& str, Vec3f& texColor, TEXT_ALIGN testPos, bool cache);
-
+	//! supprime du cache le texte s
 	void clearCache(const std::string& s);
+	//! supprime tous les textes de la font
 	void clearCache();
-
+	//! indique la taille en pixel du texte s
 	float getStrLen(const std::string& s);
-
+	//! crée tout le contexte graphique des fontes
 	static void createSC_context();
-
+	//! met en place la fonte de secours
 	static void initBaseFont(const std::string& ttfFileName);
 protected:
 	renderedString_struct renderString(const std::string &s, bool withBorder) const;
