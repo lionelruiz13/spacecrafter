eb9f64ac57a742b089c0e0ef6871159944eb940f
planetarium <43058983+Calvin-Ruiz@users.noreply.github.com>
2020-08-25 12:37:11 +0200

    remove commented code

--- src/vulkanModule/Uniform.cpp
+++ src/vulkanModule/Uniform.cpp
@@ -1,23 +1,6 @@
 #include "VirtualSurface.hpp"
 #include "Uniform.hpp"
 
-/*
-VkDescriptorPoolSize poolSize{};
-poolSize.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-poolSize.descriptorCount = 0;
-
-VkDescriptorPoolCreateInfo poolInfo{};
-poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
-poolInfo.poolSizeCount = 1;
-poolInfo.pPoolSizes = &poolSize;
-poolInfo.maxSets = poolSize.descriptorCount;
-
-VkDescriptorPool descriptorPool;
-if (vkCreateDescriptorPool(refDevice, &poolInfo, nullptr, &descriptorPool) != VK_SUCCESS) {
-    throw std::runtime_error("echec de la creation de la pool de descripteurs!");
-}
-*/
-
 Uniform::Uniform(VirtualSurface *_master, VkDeviceSize size, VkShaderStageFlags _stages) : master(_master), stages(_stages)
 {
     if (!_master->createBuffer(size, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, buffer, bufferMemory, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)) {
