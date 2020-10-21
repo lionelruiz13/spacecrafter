39b47433d5d49cb51631db44419a9b1666393da1
Calvin-Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2020-10-21 17:02:31 +0200

    Fix depth buffer clear

--- src/vulkanModule/CommandMgr.cpp
+++ src/vulkanModule/CommandMgr.cpp
@@ -355,7 +355,7 @@ void CommandMgr::beginRenderPass(renderPassType renderPassType, renderPassCompat
 
     std::array<VkClearValue, 2> clearValues{};
     clearValues[0].color = {0.0f, 0.f, 0.f, 0.0f};
-    clearValues[1].depthStencil = {0.0f, 0};
+    clearValues[1].depthStencil = {1.0f, 0};
     renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
     renderPassInfo.pClearValues = clearValues.data();
 
