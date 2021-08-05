6ad658112e789edbe05c97d1d1790b4862dc10ba
Association Sirius <olivier@asso-sirius.org>
2021-08-05 06:37:31 +0200

    Création FontFactory

---
+++ src/appModule/fontFactory.hpp
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2020 of the LSS Team & Association Sirius
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Spacecrafter is a free open project of the LSS team
+ * See the TRADEMARKS file for free open project usage requirements.
+ *
+ */
+
+#ifndef _FONTFACTORY_HPP_
+#define _FONTFACTORY_HPP_
+
+#include <string>
+#include <map>
+
+
+/**
+ * \file fontFactory.hpp
+ * \brief All Font Management : creation / update
+ * \author Association Sirius
+ * \version 1
+ *
+ * \class FontFactory
+ *
+ * \brief Transforms alphanumeric entry to Core Font 
+ *
+ * The purpose of this class is to analyze character strings (string name and string size) 
+ * and get to all Core Font Module aprouved s_font parameters
+ *
+ * Each planetarium has its own resolution. To harmonize the size of the fonts regardless of the resolution,
+ * we use a reference font size : FontResolution. And we apply proportionality to determine the size of the 
+ * fonts to display. 
+ *
+*/
+
+
+//! used to translate string to TARGETFONT
+enum class TARGETFONT : char {
+	CF_TEXTS, 
+	CF_PLANETS,
+	CF_CONSTELLATIONS,
+	CF_CARDINALS,
+	CF_HIPSTARS,
+	CF_NONE
+};
+
+class InitParser;
+
+class FontFactory {
+
+public:
+	friend class Core;
+
+    FontFactory(int _resolution);
+    ~FontFactory();
+	//! initialise les fontes: fichiers et tailles 
+    void init(const InitParser& conf);
+	//! crée les différentes fontes 
+    void setFont();
+	//! met à jour des fontes de Core
+	void updateFont(const std::string& targetName, const std::string& fontName, const std::string& sizeValue);
+ 
+private:
+	void setStrToTarget();
+	// Core
+
+	// Media
+
+	// Ui
+
+	// Core
+	std::string FontFileNameGeneral;			//! The font file used by default during initialization
+	std::string FontFileNamePlanet;				//! The font for the planet system
+	std::string FontFileNameConstellation;		//! The font for all asterims
+	std::string FontFileNameMenu;
+	double FontSizeGeneral;
+	double FontSizePlanet;
+	double FontSizeConstellation;
+	double FontSizeCardinalPoints;
+	// Media
+	std::string FontFileNameText;
+	double FontSizeText;
+
+
+    int m_resolution;
+	double m_fontResolution;
+	std::map< std::string, TARGETFONT> m_strToTarget;
+};
+
+
+#endif //_FONTFACTORY_HPP_
