a46a1fcb785449eda9e00cb7832a26bcd66d77dd
Association Sirius <olivier@asso-sirius.org>
2021-08-24 06:24:14 +0200

    VERSION 21.08.24

--- src/config.h
+++ src/config.h
@@ -6,8 +6,8 @@
 #define _CONFIG_H_
 
 #define PACKAGE  "spacecrafter"
-#define VERSION "21.08.09"
-#define APP_NAME "spacecrafter 21.08.09"
+#define VERSION "21.08.24"
+#define APP_NAME "spacecrafter 21.08.24"
 #define APP_LOWER_NAME "spacecrafter"
 #define USER_NAME "LSS Open Project"
 #define USER_EDITION "Linux Edition Vulkan 2021"
