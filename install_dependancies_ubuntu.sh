f2196c6ec758d85c97b10a3ccae752abbd20f330
Lionel RUIZ <lionel.ruiz@live.fr>
2022-02-12 14:11:28 +0100

    Update for Vulkan SDK and GIT for Entity Library

--- install_dependancies_ubuntu.sh
+++ install_dependancies_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install for kubuntu
+# Install for Ubuntu
 
 echo  ---------------------------------
 echo  ---                           ---
@@ -12,30 +12,38 @@ echo  ---------------------------------
 # sudo apt-get install -y libegl1-mesa-dev
 sudo apt-get install -y libglew-dev
 
-sudo apt-get install -y libsdl2-dev
+sudo apt install -y git
+sudo apt install -y libsdl2-dev
 #sudo apt-get install -y libsdl2-gfx-dev
-sudo apt-get install -y libsdl2-mixer-dev
-sudo apt-get install -y libsdl2-ttf-dev	 	
-sudo apt-get install -y libsdl2-image-dev
-sudo apt-get install -y libsdl2-net-dev
+sudo apt install -y libsdl2-mixer-dev
+sudo apt install -y libsdl2-ttf-dev	 	
+sudo apt install -y libsdl2-image-dev
+sudo apt install -y libsdl2-net-dev
 #sudo apt-get install -y libfreetype6-dev
 
-sudo apt-get install -y gettext
-sudo apt-get install -y make
-sudo apt-get install -y g++
-sudo apt-get install -y cmake
+sudo apt install -y gettext
+sudo apt install -y make
+sudo apt install -y g++
+sudo apt install -y cmake
 #player vidéo
-sudo apt-get install -y libavcodec-dev
-sudo apt-get install -y libavformat-dev
-sudo apt-get install -y libavutil-dev
-sudo apt-get install -y libswscale-dev
-sudo apt-get install -y libavfilter-dev
-sudo apt-get install -y libswresample-dev 
+sudo apt install -y libavcodec-dev
+sudo apt install -y libavformat-dev
+sudo apt install -y libavutil-dev
+sudo apt install -y libswscale-dev
+sudo apt install -y libavfilter-dev
+sudo apt install -y libswresample-dev 
 
-sudo apt-get install -y mplayer
+sudo apt install -y mplayer
 
-sudo apt-get install -y vulkan-tools
-sudo apt-get install -y libvulkan-dev
-sudo apt-get install -y vulkan-validationlayers-dev
+sudo apt install -y vulkan-tools
+sudo apt install -y libvulkan-dev
+sudo apt install -y vulkan-validationlayers-dev
+
+wget -qO - http://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
+sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-focal.list http://packages.lunarg.com/vulkan/lunarg-vulkan-focal.list
+sudo apt update
+sudo apt install -y vulkan-sdk
 
 echo Install completed.
+
+
