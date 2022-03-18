35e32d2888a8f705e8b345c5d67e40857a007fd1
Calvin-Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2022-03-18 14:24:51 +0100

    Link s_texture release memory to EntityCore release memory

--- src/main.cpp
+++ src/main.cpp
@@ -310,6 +310,7 @@ int main(int argc, char **argv)
 	// For windowless usage (like NDI), don't create sdl window, sdl->getWindow() must then return nullptr.
 	s_texture::loadCache(ini->getUserDir() + "cache/", conf.getBoolean(SCS_MAIN, SCK_TEX_CACHE));
 	std::unique_ptr<VulkanMgr> vulkan = std::make_unique<VulkanMgr>(APP_LOWER_NAME, VK_MAKE_API_VERSION(0, GETV(0), GETV(3), GETV(6)), sdl->getWindow(), curMin, -curMin, QueueRequirement{2, 0, 0, 1, 1}, requiredFeatures, preferedFeatures, 256, conf.getBoolean(SCS_MAIN, SCK_DEBUG_LAYER), conf.getBoolean(SCS_MAIN, SCK_DEBUG), conf.getBoolean(SCS_MAIN, SCK_LOG), ini->getUserDir() + "cache/", 3, VK_IMAGE_USAGE_TRANSFER_SRC_BIT, false, cLog::writeECLog);
+	vulkan->setReleaseUnusedMemoryCustomAction(&s_texture::releaseUnusedMemory);
 	std::unique_ptr<App> app = std::make_unique<App>(sdl.get());
 
 	// Register custom suspend and term signal handers
