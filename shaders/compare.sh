d1522f5a988e41a6d3bf619bf43116b1e2910e9a
Calvin-Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2022-10-18 09:32:24 +0200

    Update compile.sh to avoid unneeded recompilation

--- shaders/compare.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Files not compiled :"
-((echo "ls src"; ls compiled) | sed -z "s/\n/ | grep -v /g"; echo) | sed -z "s/ | grep -v \n/\n/g" | sed "s/\.spv//g" | bash | grep -v "\.glsl"
--- shaders/compile.sh
+++ shaders/compile.sh
@@ -6,48 +6,66 @@ echo "================ VERTEX SHADER ================"
 for src in *.vert
 do
     dst="../compiled/$src.spv"
-    echo "compile $src"
-    glslc $src -o $dst -I . --target-env="vulkan1.1"
+    if [ $(date -r $src +%s) != "$(date -r $dst +%s)" ]
+    then
+        echo "compile $src"
+        glslc $src -o $dst -I . --target-env="vulkan1.1" && touch -cmt "$(date -r $src +%Y%m%d%H%M.%S)" $dst
+    fi
 done
 echo "==============================================="
 echo "=============== FRAGMENT SHADER ==============="
 for src in *.frag
 do
     dst="../compiled/$src.spv"
-    echo "compile $src"
-    glslc $src -o $dst -I . --target-env="vulkan1.1"
+    if [ $(date -r $src +%s) != "$(date -r $dst +%s)" ]
+    then
+        echo "compile $src"
+        glslc $src -o $dst -I . --target-env="vulkan1.1" && touch -cmt "$(date -r $src +%Y%m%d%H%M.%S)" $dst
+    fi
 done
 echo "==============================================="
 echo "=============== GEOMETRY SHADER ==============="
 for src in *.geom
 do
     dst="../compiled/$src.spv"
-    echo "compile $src"
-    glslc $src -o $dst -I . --target-env="vulkan1.1"
+    if [ $(date -r $src +%s) != "$(date -r $dst +%s)" ]
+    then
+        echo "compile $src"
+        glslc $src -o $dst -I . --target-env="vulkan1.1" && touch -cmt "$(date -r $src +%Y%m%d%H%M.%S)" $dst
+    fi
 done
 echo "==============================================="
 echo "========== TESSELATION CONTROL SHADER ========="
 for src in *.tesc
 do
     dst="../compiled/$src.spv"
-    echo "compile $src"
-    glslc $src -o $dst -I . --target-env="vulkan1.1"
+    if [ $(date -r $src +%s) != "$(date -r $dst +%s)" ]
+    then
+        echo "compile $src"
+        glslc $src -o $dst -I . --target-env="vulkan1.1" && touch -cmt "$(date -r $src +%Y%m%d%H%M.%S)" $dst
+    fi
 done
 echo "==============================================="
 echo "======== TESSELATION EVALUATION SHADER ========"
 for src in *.tese
 do
     dst="../compiled/$src.spv"
-    echo "compile $src"
-    glslc $src -o $dst -I . --target-env="vulkan1.1"
+    if [ $(date -r $src +%s) != "$(date -r $dst +%s)" ]
+    then
+        echo "compile $src"
+        glslc $src -o $dst -I . --target-env="vulkan1.1" && touch -cmt "$(date -r $src +%Y%m%d%H%M.%S)" $dst
+    fi
 done
 echo "==============================================="
 echo "======== COMPUTE SHADER ========"
 for src in *.comp
 do
     dst="../compiled/$src.spv"
-    echo "compile $src"
-    glslc $src -o $dst -I . --target-env="vulkan1.1"
+    if [ $(date -r $src +%s) != "$(date -r $dst +%s)" ]
+    then
+        echo "compile $src ($(date -r $src +%s) != $(date -r $dst +%s)"
+        glslc $src -o $dst -I . --target-env="vulkan1.1" && touch -cmt "$(date -r $src +%Y%m%d%H%M.%S)" $dst
+    fi
 done
 echo "==============================================="
   exit 0
