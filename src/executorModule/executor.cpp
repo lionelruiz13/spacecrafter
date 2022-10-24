fbaa8f8f8ee7fce5b4de503d60768768c308246d
Ramdane Martin <martin.ramdane@epitech.eu>
2022-10-24 09:31:40 +0200

    remove change in executorModule

--- src/executorModule/executor.cpp
+++ src/executorModule/executor.cpp
@@ -64,11 +64,7 @@ void Executor::updateMode(double altitude)
 {
     if (currentMode->testValidAltitude(altitude)) {
         currentMode->onExit();
-        if (core->selected_object && currentMode == inGalaxyModule.get()) {
-            currentMode = stellarSystemModule.get();
-        } else {
-            currentMode = currentMode->getNextMode();
-        }
+        currentMode = currentMode->getNextMode();
         // Don't select an object from a different mode
         core->selected_object = Object();
         currentMode->onEnter();
