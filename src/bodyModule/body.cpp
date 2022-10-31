df37a51f7b247e164f0bd679d820d9d6e61c7d9a
Calvin-Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2022-10-31 13:06:57 +0100

    Remove atmosphereTable deduction, patch external atmosphere graphical glitch

--- src/bodyModule/body.cpp
+++ src/bodyModule/body.cpp
@@ -933,7 +933,7 @@ void Body::drawHalo(const Navigator* nav, const Projector* prj, const ToneReprod
 
 void Body::drawAtmExt(VkCommandBuffer cmd, const Projector *prj, const Navigator *nav, const Mat4f &mat, float screen_sz, bool depthTest)
 {
-    if (hasAtmosphere && screen_sz > 10) {
+    if (hasAtmosphere && screen_sz > 10 && get_angular_size(prj, nav) > 2) { // Avoid graphical glitch with too small angular size
         if (!atmExt) // AtmExt creation is fast AND never grouped
             atmExt = std::make_unique<AtmExt>(this, currentObj, atmosphereParams->tableAtmosphere);
         atmExt->draw(cmd, prj, nav, mat, eye_sun, eye_planet, one_minus_oblateness, Vec2i(bodyTesselation->getMinTesLevel(), bodyTesselation->getMaxTesLevel()), radius, radius * atmosphereParams->atmosphereRadiusFactor, screen_sz, depthTest);
