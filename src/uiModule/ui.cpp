0b4fedc0e35fa8badd88a87e4522835e6e42c275
lionelruiz13 <lionel.ruiz@live.fr>
2022-11-10 20:47:39 +0100

    Fix asterism init path

--- src/uiModule/ui.cpp
+++ src/uiModule/ui.cpp
@@ -199,7 +199,7 @@ void UI::init(const InitParser& conf)
 	coreLink->BodyOJMRemoveAll("in_universe");
 	coreLink->BodyOJMRemoveAll("in_galaxy");
 
-	coreLink->starLinesLoadData(scriptInterface->getScriptPath() + "internal/asterism_all.fab");
+	coreLink->starLinesLoadData(AppSettings::Instance()->getScriptDir() + "internal/asterism_all.fab");
 
 	core->setInitialSkyCulture();
 	core->setInitialSkyLocale();
