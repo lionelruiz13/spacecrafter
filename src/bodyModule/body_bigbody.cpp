15bc982f039744848ca05b714d9a6cf1449f256e
lionelruiz13 <lionel.ruiz@live.fr>
2022-12-26 12:22:57 +0100

    Swap to big_texture earlier

--- src/bodyModule/body_bigbody.cpp
+++ src/bodyModule/body_bigbody.cpp
@@ -525,7 +525,7 @@ Set &BigBody::getSet(float screen_sz)
             // Not handled !
             return *set;
     }
-    return (bigSet && screen_sz > 512) ? *bigSet : *set;
+    return bigSet ? *bigSet : *set;
 }
 
 void BigBody::preload(int keepFrames)
