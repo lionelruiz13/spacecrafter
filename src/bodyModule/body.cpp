60e9ca178c98be8ad3081144fa681822a130fa57
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2023-05-29 14:34:13 +0200

    Fix issue when getting inside the bounding radius

--- src/bodyModule/body.cpp
+++ src/bodyModule/body.cpp
@@ -971,7 +971,7 @@ void Body::computeDraw(const Projector* prj, const Navigator* nav)
 
     const float halfFov = prj->getFov() * (M_PI / 360);
     angularSize = atanf(radius / sqrt(distance*distance - radius*radius)) * 2;
-    isVisible = (-eye_planet[2] / distance) > cos(halfFov + atanf(boundingRadius / sqrt(distance*distance - boundingRadius*boundingRadius)));
+    isVisible = ((distance <= boundingRadius) || ((-eye_planet[2] / distance) > cos(halfFov + atanf(boundingRadius / sqrt(distance*distance - boundingRadius*boundingRadius)))));
 
     float tmp = sqrt(eye_planet[0] * eye_planet[0] + eye_planet[1] * eye_planet[1]);
     tmp = atan2f(tmp, -eye_planet[2]) / (tmp * halfFov);
