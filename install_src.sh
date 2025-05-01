ef93dda457f2dac0f7dd447e53bc1c0b5ea7ef01
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2025-05-01 10:39:45 +0200

    Enhance install_src.sh and make it executable.

--- install_src.sh
+++ install_src.sh
@@ -9,18 +9,32 @@ echo  ---       Installing          ---
 echo  ---                           ---
 echo  ---------------------------------
 
-if [ ! -d build ]
+if [ "$1" = "--update" ]
 then
-mkdir build
-cd build
+	JOBS=$2
 else
-cd build
-rm * *.* -rf
+	rm -fr build
+	mkdir build
+	JOBS=$1
 fi
 
+cd build
 git submodule update --init || (cd ../src && git clone https://github.com/Calvin-Ruiz/EntityCore.git)
 cmake .. -DCMAKE_BUILD_TYPE=Release
-nice cmake --build . $1 --config Release
+if [ "$JOBS" = "" ]
+then
+	NPROC=$(nproc)
+	JOBS=$(free --giga | grep Mem | grep -E --only-matching '[0-9]+$')
+	if [ $JOBS -lt $NPROC ]
+	then
+		echo "Using $JOBS logical cores to leave 1Go per job"
+		JOBS="-j$JOBS"
+	else
+		echo "Using all $NPROC logical cores"
+		JOBS="-j$NPROC"
+	fi
+fi
+chrt --batch 0 cmake --build . $JOBS --config Release
 sudo cmake --install . --config Release
 cd ..
 
