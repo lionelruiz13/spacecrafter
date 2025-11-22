3ad6d31bf8965c0ac9ed0022a6998754bb3d5076
lionelruiz13 <lionel.ruiz@live.fr>
2025-11-22 21:58:55 +0100

    Fix EntityCore's log management

--- src/appModule/fps.cpp
+++ src/appModule/fps.cpp
@@ -31,6 +31,7 @@
 #include <chrono>
 #include <thread>
 #include <stacktrace>
+#include "EntityCore/Core/VulkanMgr.hpp"
 
 #include "appModule/fps.hpp"
 #include "tools/log.hpp"
@@ -109,7 +110,7 @@ void Fps::watchdogMainloop()
 			std::ostringstream oss;
 			oss << stacktrace;
 			stackDumped.store(false, std::memory_order_relaxed);
-			cLog::get()->write(oss, LOG_TYPE::L_OTHER, LOG_FILE::VULKAN);
+			VulkanMgr::instance->putLog(oss.str(), LogType::LAYER);
 		}
 		#endif
 		const uint64_t currentFrame = numberFrames.load(std::memory_order_relaxed);
@@ -124,10 +125,10 @@ void Fps::watchdogMainloop()
 					#ifdef __linux__
 					kill(pid, SIGUSR1);
 					#endif
-					cLog::get()->write("Frame stall detected", LOG_TYPE::L_WARNING, LOG_FILE::VULKAN);
+					VulkanMgr::instance->putLog("Frame stall detected", LogType::WARNING);
 					break;
 				case 20:
-					cLog::get()->write("This frame stall is very long", LOG_TYPE::L_WARNING, LOG_FILE::VULKAN);
+					VulkanMgr::instance->putLog("This frame stall is very long", LogType::WARNING);
 					break;
 			}
 		}
